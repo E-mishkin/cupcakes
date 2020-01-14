75975692f0cc6e015b063b940f6bae91d9ccdf55
Evgenii Mishkin <emishkin@mail.greenriver.edu>
2020-01-13 22:59:41 -0500

    Step 3: Validation in confirmation file

---
+++ confirmation.php
@@ -0,0 +1,86 @@
+<?php
+/* This is a cupcakes.php confirmation page
+ * Jan 13, 2020
+ * Evgenii Mishkin
+*/
+
+// Turn on error reporting - this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Cupcakes confirmation file</title>
+</head>
+<body>
+
+    <div class="form-group m-5">
+
+        <?php
+            //fields
+            $name = $_POST['name'];
+            //$cupcakes = $_POST['cupcakes'];
+
+            //Validate the data
+            $isValid = true;
+
+            if (!empty($_POST['name'])) {
+
+            } else {
+                echo "<p class='text-danger'>Name is required</p>";
+                $isValid = false;
+            }
+
+            /*
+            if (!empty($cupcakes)) {
+                echo $cupcakes;
+            } else {
+                echo "<p class='text-danger'>Please check any cupcake</p>";
+            }
+            */
+
+            $cupcakesArray = array("grasshopper", "whiskey", "carrot", "salted", "red", "lemon", "tiramisu");
+            if (isset($_POST['cupcakes'])) {
+                if (in_array($_POST['cupcakes'], $cupcakesArray)) {
+
+                }
+            } else {
+                echo "<p class='text-danger'>Please check any cupcake</p>";
+                $isValid = false;
+            }
+
+            //Insert row if data is valid
+            if ($isValid) {
+                echo "<p class='mb-2'>Thank you, $name, for your order!</p><br>";
+                echo "<p>Order Summary:";
+                echo "<ul>";
+                $cupcakes = $_POST['cupcakes'];
+
+                foreach ($cupcakes as $cupcake) {
+                    echo "<li>".$cupcake."</li>";
+
+                }
+                echo "</ul>";
+
+
+            }
+        ?>
+
+    </div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
